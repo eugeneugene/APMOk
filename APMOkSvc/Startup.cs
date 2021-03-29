03899ee25f935495a74bbb79143b25767bec2e2a
eugeneugene <eugeneugene@yahoo.com>
2021-03-29 23:12:13 +0300

    Заменил NotifyIconWpf пакетом из NuGet

--- APMOkSvc/Startup.cs
+++ APMOkSvc/Startup.cs
@@ -1,37 +1,24 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ProtoBuf.Grpc.Configuration;
-using Grpc.AspNetCore;
 using ProtoBuf.Grpc.Server;
 
 namespace APMOkSvc
 {
     internal class Startup
     {
-        private IConfiguration Configuration { get; }
-        private IHostEnvironment Environment { get; }
-
-        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        public Startup()
         {
-            Configuration = configuration;
-            Environment = environment;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddCodeFirstGrpc(config =>
             {
                 config.ResponseCompressionLevel = System.IO.Compression.CompressionLevel.Optimal;
             });
-            
+
             services.AddSingleton<IAPMService, APMService>();
             services.AddCodeFirstGrpcReflection();
         }
--- APMTest/Program.cs
+++ APMTest/Program.cs
@@ -5,7 +5,7 @@ namespace APMTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
             try
             {
