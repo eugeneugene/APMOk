8ab4f86f47655f40f447047ed7f30c5581942129
eugeneugene <eugeneugene@yahoo.com>
2021-04-29 17:51:08 +0300

    Ищу решения для конфига

---
+++ Tests/ConfigTest/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Config.Net;
+
+namespace ConfigTest
+{
+    internal class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            CurrentEnvironment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment CurrentEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var path = "Mysettings.json";
+            IMySettings settings = new ConfigurationBuilder<IMySettings>().UseJsonConfig(path).Build();
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
+    }
+}
